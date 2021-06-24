b18c183a0f70b73cb253fd94193a2ad157a97be5
HaoWang95 <alanwang0028@gmail.com>
2021-06-24 19:09:08 +1000

    bug fix using storybook cli

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,11 @@
 module.exports = {
-  // "stories": [
-  //   "../src/**/*.stories.mdx",
-  //   "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  // ],
-
-  "stories":[".../src/components/**/*.stories.js"],
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ],
+  ]
 }
