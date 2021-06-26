13f2bc1a6bde4a5eeb7988a3ed6ca371e82a97f2
HaoWang95 <alanwang0028@gmail.com>
2021-06-26 11:27:16 +1000

    task update

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Button } from './Button';
 
 export default {
-  title: 'Example/Button',
+  title: 'StoryBook/Button',
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -20,6 +20,7 @@ Primary.args = {
 
 export const Secondary = Template.bind({});
 Secondary.args = {
+  //primary: true,
   label: 'Button',
 };
 
